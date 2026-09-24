1ca55835d4eb058ad20a6727b12a72854bff698e
An1 <ann-1@qq.com>
2026-09-24 21:26:59 +0800

    Revert "弹窗统一动效: 方案 B「生长」(用户选定), 收口到 DialogChrome"
    This reverts commit d15375939b5fc2c92d3e2dc93b1e52615189949f.

--- config-ui-avalonia/Views/PluginMarketWindow.axaml.cs
+++ config-ui-avalonia/Views/PluginMarketWindow.axaml.cs
@@ -15,7 +15,7 @@ public partial class PluginMarketWindow : Window
     {
         InitializeComponent();
         ComponentFocusRing.Attach(this); // 焦点环最内层转移 (替代 :focus-within)
-        Services.Win32.DialogChrome.Apply(this, Services.DialogMotion.Profile.Large);
+        Services.Win32.DialogChrome.Apply(this);
         Closed += (_, _) => UnsubscribeLanguage();
         Services.Win32.DialogPlacer.AttachAutoCenter(this); // 统一定位: 异步内容长高后自动重居中
         I18n.Changed += OnLanguageChanged;
